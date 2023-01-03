8ee9d02708357a9b187c1c801f7f6ac7e0054f3f
emhofer <p.emhofer@gmail.com>
2023-01-03 10:42:17 +0100

    started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+
+let sampleData = [1, 2, 3, 4, 5];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {sampleData.map((item) => {
+        console.log(item);
+        return (
+          <>
+            <div className="card">
+              <h3>Card Title</h3>
+              <p>Card Text</p>
+              <p>x Points | x Comments</p>
+            </div>
+            <hr />
+          </>
+        );
+      })}
     </div>
   );
 }
